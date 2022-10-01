d1e7bec38995e9eb494ea798c38d537ed0289c28
Rene Zeldenthuis <rene@zeldenthuis.eu>
2022-10-01 21:53:36 +0200

    casting

--- include/timezonelookup.h
+++ include/timezonelookup.h
@@ -5,5 +5,9 @@
 
 const char *lookup_timezone_posix_tz(const char *zone_name)
 {
-    return bsearch((void*)zone_name, (void*)timezones, sizeof(timezones)/sizeof(timezone_t), sizeof(timezone_t), [](const void *key, const void *element) { return strcmp(key, element);});
+    return bsearch((void *)zone_name, (void *)timezones, sizeof(timezones) / sizeof(timezone_t), sizeof(timezone_t),
+                   [](const void *key, const void *element)
+                   {
+                       return strcmp(((const timezone_t *)key)->zone_name, ((const timezone_t *)element)->zone_name);
+                   });
 }
