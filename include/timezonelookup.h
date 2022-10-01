69591a59770d5b4adc04dbbe41c13aebfc7a75fd
Rene Zeldenthuis <rene@zeldenthuis.eu>
2022-10-01 23:06:12 +0200

    bsearch

--- include/timezonelookup.h
+++ include/timezonelookup.h
@@ -3,11 +3,13 @@
 #include <timezonedb.h>
 #include <stdlib.h>
 
-const char *lookup_timezone_posix_tz(const char *zone_name)
+const timezone_t *lookup_timezone_posix_tz(const char *zone_name)
 {
-    return bsearch((void *)zone_name, (void *)timezones, sizeof(timezones) / sizeof(timezone_t), sizeof(timezone_t),
-                   [](const void *key, const void *element)
-                   {
-                       return strcmp(((const timezone_t *)key)->zone_name, ((const timezone_t *)element)->zone_name);
-                   });
+    // Array must be sorted on item to do a bsearch
+    timezone_t key = {zone_name = zone_name};
+    return (timezone_t *)bsearch(&key, timezones, sizeof(timezones) / sizeof(timezone_t), sizeof(timezone_t),
+                                 [](const void *e1, const void *e2)
+                                 {
+                                     return strcmp(((timezone_t *)e1)->zone_name, ((timezone_t *)e2)->zone_name);
+                                 });
 }
